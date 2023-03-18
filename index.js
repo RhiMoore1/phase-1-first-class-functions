423b78127e588d464976cb88325002010e315614
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-03-18 06:52:19 -0700

    completes lab

---
+++ index.js
@@ -0,0 +1,45 @@
+ 
+ 
+ // takes a callback as an argument
+ // call the callback function
+ function receivesAFunction(callback) {
+    callback();
+}
+receivesAFunction();
+
+
+
+// take no arguments
+// return a named function
+function returnsANamedFunction() {
+    function namedFunction() {
+       console.log('hello');
+    }
+    return namedFunction;
+}
+returnsANamedFunction();
+
+
+// takes no arguments
+// returns an anonymous function
+function returnsAnAnonymousFunction() {
+    return (function() {
+        console.log('hello');  // anonymous function
+    });
+}
+returnsAnAnonymousFunction();
+
+
+// takes no arguments
+// returns an anonymous function using arrow function
+
+/*
+function returnsAnAnonymousFunction() {
+    return (() => {
+        console.log('hi');
+    });
+}
+returnsAnAnonymousFunction();
+*/
+
+
